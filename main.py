669fdf727f8decbf057a9d5ed37e5c284ac4af13
ExtraE113 <ezra@manmans.com>
2020-07-18 18:12:06 -0700

    attempt to work around known gcf bug https://issuetracker.google.com/issues/155215191#comment1

--- main.py
+++ main.py
@@ -54,35 +54,38 @@ def donate_endpoint(request):
 # creates a square customer if the user doesn't already have one stored
 # todo test- is this even working?
 def add_cof(request):
-	# todo idempotency
-	# todo if there is already
-	id_token = None
-	request_json = request.get_json()
-	if request.args and 'token' in request.args:
-		id_token = request.args.get('token')
-	elif request_json and 'token' in request_json:
-		id_token = request_json['token']
+	try:
+		# todo idempotency
+		# todo if there is already
+		id_token = None
+		request_json = request.get_json()
+		if request.args and 'token' in request.args:
+			id_token = request.args.get('token')
+		elif request_json and 'token' in request_json:
+			id_token = request_json['token']
 
-	if id_token is None:
-		return "error: no token supplied"
+		if id_token is None:
+			return "error: no token supplied"
 
-	nonce = None
-	if request.args and 'nonce' in request.args:
-		nonce = request.args.get('nonce')
-	elif request_json and 'nonce' in request_json:
-		nonce = request_json['nonce']
+		nonce = None
+		if request.args and 'nonce' in request.args:
+			nonce = request.args.get('nonce')
+		elif request_json and 'nonce' in request_json:
+			nonce = request_json['nonce']
 
-	if nonce is None:
-		return "error: no nonce supplied"
+		if nonce is None:
+			return "error: no nonce supplied"
 
-	customer_id = google_square_integration_utils.get_square_customer_id_from_id_token(id_token)
-	print(customer_id)
-	if customer_id is None:
-		print("here")
-		result = square_client.create_customer(
-			email_address=google_square_integration_utils.get_user_from_id_token(id_token).email)
-		print(result)
-		customer_id = result["id"]
+		customer_id = google_square_integration_utils.get_square_customer_id_from_id_token(id_token)
+		print(customer_id)
+		if customer_id is None:
+			print("here")
+			result = square_client.create_customer(
+				email_address=google_square_integration_utils.get_user_from_id_token(id_token).email)
+			print(result)
+			customer_id = result["id"]
 
-	square_client.store_card_on_file(nonce=nonce, customer_id=customer_id)
-	return str(google_square_integration_utils.update_square_customer_id_by_id_token(id_token, customer_id))
+		square_client.store_card_on_file(nonce=nonce, customer_id=customer_id)
+		return str(google_square_integration_utils.update_square_customer_id_by_id_token(id_token, customer_id))
+	except BaseException as e:
+		print(e)
